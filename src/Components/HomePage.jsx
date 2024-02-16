afc91436b13da2fce4acfdd19d35cf167666ed88
DigitalForge <DigitalForge@Matthews-MacBook-Pro.local>
2024-02-16 16:47:59 -0600

    hamburger icon created

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,15 @@
+import "./HomePage.css";
 const HomePage = () => {
+  function toggleMenu() {
+    const icon = document.getElementById("hamburger-icon");
+    icon.classList.toggle("open");
+  }
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div id="hamburger-icon" onClick={toggleMenu}>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
