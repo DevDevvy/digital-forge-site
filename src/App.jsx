82b3088683036cfdae33c1b6643cee2f345d31d0
DigitalForge <DigitalForge@Matthews-MacBook-Pro.local>
2024-02-16 16:02:06 -0600

    homepage works

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import HomePage from "./HomePage";
+import HomePage from "./Components/HomePage";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route exact path="/" element={<HomePage />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
