fb5e66e9e4d0a94bfbd817e94093aa42172589db
randalldigitalforge <randall@digitalforge-tek.com>
2024-04-07 15:54:51 -0700

    getting download right with encoding

--- src/Pages/eBusinessCards/BusinessCard.jsx
+++ src/Pages/eBusinessCards/BusinessCard.jsx
@@ -19,8 +19,29 @@ const EBusinessCard = () => {
   }, []);
 
   const downloadVCard = () => {
+    // Example VCF content
+    const vCardData = `BEGIN:VCARD
+  VERSION:3.0
+  FN:${person.name}
+  ORG:Digital Forge
+  TEL;TYPE=WORK,VOICE:${person.phone}
+  EMAIL;TYPE=WORK,EMAIL:${person.email}
+  END:VCARD`;
+    // Convert the string to a UTF-8 byte array
+    const encoder = new TextEncoder();
+    const uint8Array = encoder.encode(vCardData);
+
+    // Convert the byte array to a Base64 string
+    const base64String = uint8Array.reduce((acc, byte) => {
+      return acc + String.fromCharCode(byte);
+    }, "");
+
+    // Use btoa to convert the binary string to Base64
+    const base64VCard = btoa(base64String);
+
+    // Create a data URI for the download
     const link = document.createElement("a");
-    link.href = person.vcfFile;
+    link.href = `data:text/vcard;base64,${base64VCard}`;
     link.download = `${name}.vcf`;
     document.body.appendChild(link);
     link.click();
