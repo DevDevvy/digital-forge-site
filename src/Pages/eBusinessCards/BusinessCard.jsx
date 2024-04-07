a4f72270a238d00336d77147a03b494a8a61f26c
randalldigitalforge <randall@digitalforge-tek.com>
2024-04-07 15:23:55 -0700

    eBusiness cards started. Sample at /about/card/randall

---
+++ src/Pages/eBusinessCards/BusinessCard.jsx
@@ -0,0 +1,89 @@
+import styles from "./BusinessCard.module.css"; // Import CSS module
+import { useState, useEffect } from "react";
+
+import { useParams } from "react-router-dom";
+import people from "./cardData";
+import emailIcon from "../../assets/email.svg";
+import twitterIcon from "../../assets/twitterLogo.svg";
+import logo from "../../assets/logo.png";
+
+const EBusinessCard = () => {
+  let { name } = useParams();
+  const person = people[name.toLowerCase()];
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsFlipped(true), 2000); // Flips card
+  }, []);
+
+  const downloadVCard = () => {
+    const link = document.createElement("a");
+    link.href = person.vcfFile;
+    link.download = `${name}.vcf`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <>
+      <div className={styles.cardParentContainer}>
+        <div className={styles.cardContainer}>
+          <div className={`${styles.card} ${isFlipped ? styles.flipped : ""}`}>
+            <div className={styles.front}>
+              {/* Splash Screen with Logo */}
+              <img id={styles.cardLogo} src={logo} alt="Company Logo" />
+            </div>
+            <div className={styles.back}>
+              <div className={styles.profile}>
+                <img
+                  src={person.profileImage}
+                  alt={`${name}'s profile`}
+                  className={styles.profileImage}
+                />
+                <h2>{name}</h2>
+                <p>{person.title} at Digital Forge</p>
+              </div>
+              <div className={styles.socialDiv}>
+                {person.email ? (
+                  <a href={`mailto:${person.email}`}>
+                    <img
+                      className={styles.socialIcon}
+                      src={emailIcon}
+                      alt="Email"
+                    />
+                  </a>
+                ) : (
+                  ""
+                )}
+                {person.twitter ? (
+                  <a href={person.twitter}>
+                    <img
+                      src={twitterIcon}
+                      className={styles.socialIcon}
+                      alt="Twitter"
+                    />
+                  </a>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className={styles.download}>
+                <button
+                  onClick={downloadVCard}
+                  className={styles.downloadButton}
+                >
+                  Download Contact
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EBusinessCard;
---
+++ src/Pages/eBusinessCards/cardData.js
@@ -0,0 +1,21 @@
+const cardData = {
+    randall: {
+        name: "Randall",
+        title: "CTO",
+        email: "randall@digitalforge-tek.com",
+        twitter: "https://twitter.com/randalltmusic",
+        company: "Digital Forge",
+        profileImage: "/src/assets/randall.png",
+        vcfFile: "/src/assets/Randall.vcf",
+    },
+    mac: {
+        name: "Jane Doe",
+        title: "Product Manager",
+        company: "Creative Solutions Ltd.",
+        profileImage: "/path/to/jane-doe.jpg",
+        vcfFile: "/path/to/jane-doe.vcf",
+    },
+    // Add more people as needed
+};
+
+export default cardData;
