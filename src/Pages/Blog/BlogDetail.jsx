89149f4ae93fd8bc3a15f44996bbc487e128630e
randalldigitalforge <randall@digitalforge-tek.com>
2024-04-27 21:50:00 -0700

    headers and footer added

--- src/Pages/Blog/BlogDetail.jsx
+++ src/Pages/Blog/BlogDetail.jsx
@@ -1,26 +1,32 @@
 import { useParams } from "react-router-dom";
 import blogs from "./blogs";
 import "./BlogDetail.css";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
 const BlogDetail = () => {
   const { id } = useParams();
   const blog = blogs.find((blog) => blog.id === id);
   return (
-    <div className="blog-detail">
-      <div className="main-content">
-        <img src={blog.mainImage} alt="Main" className="main-blog-image" />
+    <>
+      <Header />
+      <div className="blog-detail">
+        <div className="main-content">
+          <img src={blog.mainImage} alt="Main" className="main-blog-image" />
+          <div
+            dangerouslySetInnerHTML={{
+              __html: blog.content.split("</p>")[0] + "</p>",
+            }}
+            className="first-paragraph"
+          />
+        </div>
         <div
           dangerouslySetInnerHTML={{
-            __html: blog.content.split("</p>")[0] + "</p>",
+            __html: blog.content.substring(blog.content.indexOf("</p>") + 4),
           }}
-          className="first-paragraph"
         />
       </div>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: blog.content.substring(blog.content.indexOf("</p>") + 4),
-        }}
-      />
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- src/Pages/Blog/BlogList.jsx
+++ src/Pages/Blog/BlogList.jsx
@@ -1,27 +1,33 @@
 import blogs from "./blogs";
 import "./BlogList.css";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
 const BlogList = () => {
   return (
-    <div className="blog-list">
-      {blogs.map((blog, index) => (
-        <div
-          key={blog.id}
-          className={`blog-preview ${index % 2 === 0 ? "normal" : "reverse"}`}
-        >
-          <img src={blog.image} alt="Preview" className="blog-image" />
-          <div className="blog-info">
-            <h1>{blog.title}</h1>
-            <p>{blog.snippet}</p>
-            <button
-              className="read-more"
-              onClick={() => (window.location = `/blog/${blog.id}`)}
-            >
-              Read More
-            </button>
+    <>
+      <Header />
+      <div className="blog-list">
+        {blogs.map((blog, index) => (
+          <div
+            key={blog.id}
+            className={`blog-preview ${index % 2 === 0 ? "normal" : "reverse"}`}
+          >
+            <img src={blog.image} alt="Preview" className="blog-image" />
+            <div className="blog-info">
+              <h1>{blog.title}</h1>
+              <p>{blog.snippet}</p>
+              <button
+                className="read-more"
+                onClick={() => (window.location = `/blog/${blog.id}`)}
+              >
+                Read More
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <Footer />
+    </>
   );
 };
 
